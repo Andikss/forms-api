e378ab4b0143788a0d9f1e0c349d89c7065583d0
Andikss <andika.dwi.saputra.7925@gmail.com>
2024-04-11 16:12:44 +0700

    feat: create token validation middleware

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,17 @@
 <?php
 
+use App\Http\Controllers\Auth\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+
+Route::prefix('v1')->group(function () {
+    Route::group(['prefix' => 'auth'], function () {
+        Route::post('/login', [AuthController::class, 'login'])->name('login');
+        Route::get('/logout', [AuthController::class, 'logout'])->name('logout')->middleware('token.validate');
+    });
+});
