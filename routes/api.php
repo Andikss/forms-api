06a885ab78d1dd16cda6cf1b9f9ca84d6b916b84
Andikss <andika.dwi.saputra.7925@gmail.com>
2024-04-11 17:08:18 +0700

    update: change app timezone to asia/jakarta

--- routes/api.php
+++ routes/api.php
@@ -4,21 +4,20 @@ use App\Http\Controllers\Auth\AuthController;
 use App\Http\Controllers\Main\FormController;
 use App\Http\Controllers\Main\QuestionController;
 use App\Http\Controllers\Main\ResponseController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1')->group(function () {
 
+    # Auth Endpoints
     Route::group(['prefix' => 'auth'], function () {
-        # Auth Endpoints
         Route::post('/login', [AuthController::class, 'login'])->name('login');
         Route::get('/logout', [AuthController::class, 'logout'])->name('logout')->middleware('token.validate');
     });
 
+    # Forms Endpoints
     Route::group(['prefix' => 'forms', 'middleware' => 'token.validate'], function () {
-        # Forsm Endpoints
         Route::get('/', [FormController::class, 'index'])->name('forms.index');
-        Route::get('/{form_detail}', [FormController::class, 'detail'])->name('forms.detail');
+        Route::get('/{form_slug}', [FormController::class, 'detail'])->name('forms.detail');
         Route::post('/', [FormController::class, 'store'])->name('forms.store');
 
         # Question Endpoints
