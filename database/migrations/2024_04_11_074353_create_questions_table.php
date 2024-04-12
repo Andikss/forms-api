fb2ee550b204e18e792d50a504f231a48a428e5b
Andikss <andika.dwi.saputra.7925@gmail.com>
2024-04-12 10:53:39 +0700

    fix: rename question table into questions table

--- database/migrations/2024_04_11_074353_create_questions_table.php
+++ database/migrations/2024_04_11_074353_create_questions_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('question', function (Blueprint $table) {
+        Schema::create('questions', function (Blueprint $table) {
             $table->id();
             $table->foreignId('form_id')->constrained('forms')->cascadeOnDelete();
             $table->string('name');
@@ -27,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('question');
+        Schema::dropIfExists('questions');
     }
 };
