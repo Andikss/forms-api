c2b66d6a81cf62504abff8bbf01aaa087414fb8c
Andikss <andika.dwi.saputra.7925@gmail.com>
2024-04-13 08:18:39 +0700

    create response store request

---
+++ app/Http/Requests/Main/Response/StoreRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests\Main\Response;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Support\Facades\Auth;
+
+class StoreRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return Auth::check();
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'answers' => 'required|array'
+        ];
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json(['message' => $validator->errors()], 422));
+    }
+}
